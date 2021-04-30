140d91abfa4ff472e23093adb1bf934c72018726
FlameDuder <flamelord01@gmail.com>
2021-04-30 16:47:52 -0400

    Commit #11

--- src/main/java/main/BotMain.java
+++ src/main/java/main/BotMain.java
@@ -14,7 +14,7 @@ public class BotMain {
 
     public static void main(String[] args) throws Exception{
 
-        JDABuilder builder = new JDABuilder().createLight("ODE5NTc1NzMyOTAzNjA4MzIx.YEonYQ.--EXRkg3jFvQS5hNL0K0sLveSW0");
+        JDABuilder builder = new JDABuilder().createLight("");
         builder.setChunkingFilter(ChunkingFilter.ALL);
         builder.enableIntents(GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_PRESENCES);
         builder.setMemberCachePolicy(MemberCachePolicy.ALL);
